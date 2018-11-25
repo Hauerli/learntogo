5e344f471ff4148b11d08db94fd31acdc85605a7
Hauerli <Yb4xNXHDVk2oQ>
2018-11-25 16:26:27 +0100

    25112018

--- Ninja4/level8/main.go
+++ Ninja4/level8/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
+	m := map[string][]string{
+		"bond_james":      []string{"Shaken", "Martinis", "Women"},
+		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
+		"no_dr":           []string{"Being Evil", "Ice Cream", "Sunsets"},
+	}
+	for k, v := range m {
+		fmt.Println("This is the Record for ", k)
+		for i, v2 := range v {
+			fmt.Println("The Index is", i, "\t this is the Value", v2)
+		}
+	}
 }
