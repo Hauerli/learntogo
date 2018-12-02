b7f48cbce8eb91d4a924371053d6c6482fe027c7
Maximilian Hauer <maxihauer@gmx.de>
2018-12-02 22:20:20 +0100

    m

--- Ninja6/level7/main.go
+++ Ninja6/level7/main.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+func findType(i interface{}) {
+	switch i.(type) {
+	case int:
+		fmt.Println("Its an INT: ", i.(int))
+	case string:
+		fmt.Println("Its an String: ", i.(string))
+	default:
+		fmt.Println("It is not clear")
+	}
+}
+
 func main() {
-	fmt.Print("Hello World")
+
+	findType("Hello")
+	findType(64)
+	findType(34.2)
 }
